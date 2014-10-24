da28eb7d2d313e9ca0bf753b802f45eaa3c8640b
Lars Bjørlykke Kristiansen <lars.bjorlykke.kristiansen@gmail.com>
2014-10-24 18:52:56 +0200

    added no_tb. Results from busn-alloc branch

--- kerneldir.sh
+++ kerneldir.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
-for tb in tb_on tb_off; do
+if [ $# -eq 0 ]
+	then
+	KERNEL=$(uname-rv)
+else
+	KERNEL=$1
+fi
+for tb in tb_on tb_off no_tb; do
 	for acpi in acpi_on acpi_off; do
 		for boot in boot_in boot_out; do
 			for event in add boot removed; do
-				mkdir -p "$(uname -rv)/$tb/$acpi/$boot/$event"
-				touch "$(uname -rv)/$tb/$acpi/$boot/$event/.ignore"
+				mkdir -p "$KERNEL/$tb/$acpi/$boot/$event"
+				touch "$KERNEL/$tb/$acpi/$boot/$event/.ignore"
 			done
 		done
 	done
