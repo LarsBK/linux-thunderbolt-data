6c97f72361f391ef2b26b366050caff95e4eb5e4
Lars Bjørlykke Kristiansen <lars.bjorlykke.kristiansen@gmail.com>
2014-10-30 15:56:11 +0100

    fixes to scripts, added 3.17 dolphin+magma

--- dump.sh
+++ dump.sh
@@ -4,7 +4,19 @@ then
 	echo "Please run me as root. Idiot"
 	exit
 fi
-dmesg > dmesg.txt
+if [ $# -eq 0 ]
+	then
+	echo "please give dir as first arg"
+	exit
+else
+	DIR="$@"
+fi
+echo "$DIR"
+cd "$DIR"
+pwd
+ls -la
+whoami
+dmesg > ./dmesg.txt
 lspci -vvv > lspci.txt
 lspci -tv > tree.txt
 chmod 644 dmesg.txt lspci.txt tree.txt
--- kerneldir.sh
+++ kerneldir.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 if [ $# -eq 0 ]
 	then
-	KERNEL=$(uname-rv)
+	KERNEL=$(uname -rv)
 else
 	KERNEL=$1
 fi
+echo $KERNEL
 for tb in tb_on tb_off no_tb; do
 	for acpi in acpi_on acpi_off; do
 		for boot in boot_in boot_out; do
